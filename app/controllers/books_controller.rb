18e0dd1a095e440bfb2f00bd3e0a7e6b0129b483
emanuelhailu <emparalhailu@yahoo.com>
2015-12-24 16:17:48 +0300

    defined books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,27 @@
 class BooksController < ApplicationController
+
+def index
+end
+
+def show
+end
+def new
+end
+
+def Create
+end
+
+
+def edit
+end
+
+def destroy
+end
+
+def update
+end
+
+
+
+
 end
